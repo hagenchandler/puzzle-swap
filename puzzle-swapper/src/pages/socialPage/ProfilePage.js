f86cce2cb4eee415298034de6c56cb8c1723370b
Chandler <chandlerjhagen@gmail.com>
2024-12-12 19:03:57 -0600

    updated file dependency

--- puzzle-swapper/src/pages/socialPage/ProfilePage.js
+++ puzzle-swapper/src/pages/socialPage/ProfilePage.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import {Formik, Form, Field } from 'formik';
+import Avatar from '@sabfry/avatarium';
 import './ProfilePage.css';
 
 const ProfilePage = () => {
